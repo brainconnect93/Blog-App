19449e72339398830b74b1de0a6b581dfd913900
brainconnect93 <brainconnect0@gmail.com>
2022-09-29 16:55:08 +0100

    Created some parameter inline with project requirements

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,13 @@
 class Post < ApplicationRecord
-    belongs_to: author, class_name: 'User'
-    has_many: comments
-    has_many: likes
+    belongs_to :user
+    has_many :comments
+    has_many :likes
+
+    def updates_posts_counter
+        posts.increment!(:PostsCounter)
+    end
+
+    def recent_comments
+        comments.order(created_at: :desc).last(5)
+    end
 end

eec10311dc89f55d381beeabc32fb30c886bfc45
brainconnect93 <brainconnect0@gmail.com>
2022-10-03 21:37:19 +0100

    Fix all linters errors

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationRecord
-    def index
-        @user = User.all
-    end
+  def index
+    @user = User.all
+  end
 
-    def show
-        @user = User.create(param[:id])
-    end
+  def show
+    @user = User.create(param[:id])
+  end
 end

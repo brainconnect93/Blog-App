ae571c741093b2715e0581ed283971d81f650700
brainconnect93 <brainconnect0@gmail.com>
2022-10-04 18:06:35 +0100

    Make changes to the initial code

--- app/controllers/post_controller.rb
+++
@@ -1,9 +0,0 @@
-class PostsController < ApplicationController
-  def index
-    @post = Post.all
-  end
-
-  def show
-    @post = Post.create(params[:id])
-  end
-end
--- app/controllers/user_controller.rb
+++
@@ -1,9 +0,0 @@
-class UsersController < ApplicationRecord
-  def index
-    @user = User.all
-  end
-
-  def show
-    @user = User.create(param[:id])
-  end
-end

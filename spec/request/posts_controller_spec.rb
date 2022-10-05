6fc36c5333122a93c9d5f062097c4bf5e349f56f
brainconnect93 <brainconnect0@gmail.com>
2022-10-05 16:22:43 +0100

    Initialize testing for show in the posts_controller

--- spec/request/posts_controller_spec.rb
+++ spec/request/posts_controller_spec.rb
@@ -19,4 +19,15 @@ describe PostsController, type: :request do
             expect(response.body).to include('I come in peace')
         end
     end
+
+    describe "GET '/show' page" do
+        before(:all) do
+            @link = get '/users/:id/posts/:id';
+            get '/users/:id/posts/:id';
+        end
+
+        it 'returns a 200 status code' do
+            expect(@link).to eq(200)
+        end
+    end
 end

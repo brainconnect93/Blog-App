d8040fbaf935901b84d075401607b81c0dd31944
brainconnect93 <brainconnect0@gmail.com>
2022-10-05 16:20:11 +0100

    Complete testing for index in the posts_controller

--- spec/request/posts_controller_spec.rb
+++ spec/request/posts_controller_spec.rb
@@ -10,5 +10,13 @@ describe PostsController, type: :request do
         it 'returns a 200 status code' do
             expect(@link).to eq(200)
         end
+
+        it 'renders the index template' do
+            expect( get '/users/:id/posts').to render_template('posts/index')
+        end
+
+        it 'loads the posts to includes correct placeholder text' do
+            expect(response.body).to include('I come in peace')
+        end
     end
 end

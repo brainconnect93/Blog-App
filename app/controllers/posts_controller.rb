7393db336f777c0bfec60f1eb58780ba1f0ae5c1
brainconnect93 <brainconnect0@gmail.com>
2022-10-08 14:08:40 +0100

    Initialize the form method in the controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,26 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    # @post = @user.posts
+    @post = @user.posts
   end
 
   def show
     @post = Post.find(params[:id].to_i)
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params[:id])
+    respond_to do |format|
+      format.html do
+        if @post.save
+          redirect_to user_posts_path(@user), message: 'Post created successfully'
+        else
+          render :new, status: 'Error: Post not created'
+        end
+      end
+    end
 end

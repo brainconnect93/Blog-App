2ac13367e058afe349327eec7fc74476900aa09b
brainconnect93 <brainconnect0@gmail.com>
2022-10-11 23:12:07 +0100

    Add testing for index users

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -12,5 +12,21 @@ RSpec.describe User, type: :feature do
                 expect(page).to have_content(user.Name)
             end
         end
+        it 'should show the pictures of all users' do
+            @users.each do |user|
+                expect(page).to have_css("img[src*='#{user.Photo}']")
+            end
+        end
+        it 'should show number of posts of each users' do
+            @users.each do |user|
+                expect(page).to have_content(user.PostsCounter)
+            end
+        end
+        it 'should redirect to a user\'s show page when click on user' do
+            @users.each do |user|
+                click_link(user.Name)
+                expect(page).to have_current_path(user_path(user))
+            end
+        end
     end 
 end

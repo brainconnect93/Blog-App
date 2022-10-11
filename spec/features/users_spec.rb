e15e3ecd1dd23e9ebf3f06ca0144d9e18029e1d3
brainconnect93 <brainconnect0@gmail.com>
2022-10-12 00:03:57 +0100

    start the show page testing for user

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -28,5 +28,34 @@ RSpec.describe User, type: :feature do
                 expect(page).to have_current_path(user_path(user))
             end
         end
-    end 
+    end
+
+    describe 'Show page' do
+        before(:each) do
+            @user = User.create(
+                Name: 'Tom', 
+                Photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                Bio: 'Teacher from Mexico.', 
+                PostsCounter: 0
+            )
+            Post.create(
+                id: rand(1000),
+                user: @user,
+                title: 'My testing',
+                text: 'This is my testing post',
+                comments_counter: 0,
+                likes_counter: 0
+            )
+            @user = User.all
+            visit user_path(@user.first)
+        end
+
+        it 'should show user\'s picture' do
+            expect(page).to have_css("img[src*='#{@user.first.Photo}']")
+        end
+
+        it 'should show user\'s name' do
+            expect(page).to have_content(@user.first.Name)
+        end
+    end
 end

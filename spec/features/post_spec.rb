921dd9b06d513b68d61314b682cb2416a14597a5
brainconnect93 <brainconnect0@gmail.com>
2022-10-12 02:12:23 +0100

    complete the index post testing

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,12 +12,12 @@ RSpec.describe Post, type: :feature do
             @post_1 = Post.create(
                 id: rand(1000),
                 user: @user,
-                title: 'My testing',
-                text: 'This is my testing post',
+                title: 'Hello',
+                text: 'This is my first post',
                 comments_counter: 0,
                 likes_counter: 0
             )
-            Comment.create(user: @user, post: @post_1, text: 'My first comment')
+            Comment.create(user: @user, post: @post_1, text: 'Afolabi is still learning')
             @user = User.all
             visit user_posts_path(@user.ids)
         end
@@ -36,6 +36,45 @@ RSpec.describe Post, type: :feature do
 
         it 'shouldIt shows the posts text' do
             expect(page).to have_content(@post_1.text)
+        end
+
+        it 'should show the number of comments a post has' do
+            expect(page).to have_content(@post_1.comments_counter)
+        end
+
+        it 'should show the number of likes a post has' do
+            expect(page).to have_content(@post_1.likes_counter)
+        end
+
+        # it 'should redirects to the posts page when I click on a post' do
+        #     click_link 'Hello'
+        #     expect(page).to have_current_path(user_post_path(@user.ids, @post_1.id))
+        # end
+    end
+
+    describe 'Show page' do
+        before(:each) do
+            @user = User.create(
+                Name: 'Tom', 
+                Photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                Bio: 'Teacher from Mexico.', 
+                PostsCounter: 0
+            )
+            @post_1 = Post.create(
+                id: rand(1000),
+                user: @user,
+                title: 'Hello',
+                text: 'This is my first post',
+                comments_counter: 0,
+                likes_counter: 0
+            )
+            Comment.create(user: @user, post: @post_1, text: 'Afolabi is still learning')
+            @user = User.all
+            visit user_post_path(@user.ids, @post_1.id)
+        end
+
+        it 'should show the post title' do
+            expect(page).to have_content(@post_1.title)
         end
     end
 end

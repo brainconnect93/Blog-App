6fe0a1e7b3aaac040dc4878caf26bb928afad6f6
brainconnect93 <brainconnect0@gmail.com>
2022-10-14 20:20:04 +0100

    API controller is set to render for other files

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,16 +1,17 @@
 class ApiController < ActionController::API
-    
+    include ActionController::HttpAuthentication::Token::ControllerMethods
     def authorize_request
         header = request.headers['Authorization']
-        header = header.split(' ').last if header
+        header = header.split.last if header
         begin
             @decoded = AuthenticationTokenService.decode(header)
             @current_user = User.find(@decoded[:user_id])
-        rescue ActiveRecord::RecordNotFound => e
-            render json: { errors: e.message }, status: :unauthorized
         rescue JWT::DecodeError => e
             render json: { errors: e.message }, status: :unauthorized
         end
-        
+    end
+
+    def record_not_found(e)
+        render json: { errors: e.message }, status: :not_found
     end
 end

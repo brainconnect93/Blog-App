fd11dc1568f4307497fd6b68f2b12b07395e17af
brainconnect93 <brainconnect0@gmail.com>
2022-10-14 20:18:08 +0100

    Update the controller user to hold a parameter form the API

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,16 @@
+class ApiController < ActionController::API
+    
+    def authorize_request
+        header = request.headers['Authorization']
+        header = header.split(' ').last if header
+        begin
+            @decoded = AuthenticationTokenService.decode(header)
+            @current_user = User.find(@decoded[:user_id])
+        rescue ActiveRecord::RecordNotFound => e
+            render json: { errors: e.message }, status: :unauthorized
+        rescue JWT::DecodeError => e
+            render json: { errors: e.message }, status: :unauthorized
+        end
+        
+    end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  load_and_authorize_resource
   def index
     @users = User.all
   end
@@ -6,5 +8,39 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     # @recent_posts = User.includes(:comments).find(params[:id].to_i).recent_posts
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid? 
+      @user.save
+      redirect_to users_path
+    else
+      redirect_to new_users_path
+    end
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    respond_to? do |format|
+      if current_user.update(user_params)
+        format.html { redirect_to users_path, notice: 'User was successfully updated.' }
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:Name, :email, :password, :password_confirmation)
   end
 end
